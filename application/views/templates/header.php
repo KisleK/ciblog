ab564ada7f54c69bcb8c49e6083678bd9dfb2fc1
KisleK <kisle.kuhn1@gmail.com>
2019-08-22 14:33:03 -0500

    initial commit through part3

---
+++ application/views/templates/header.php
@@ -0,0 +1,51 @@
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>CodeIgniter Blog</title>
+   <link rel="stylesheet" type="text/css" href="https://bootswatch.com/4/lumen/bootstrap.min.css">
+<script src="https://cdn.ckeditor.com/4.12.1/standard/ckeditor.js"></script>
+</head>
+<body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <a class="navbar-brand" href="<?php echo base_url(); ?>">CI Blog</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarColor01">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="<?php echo base_url(); ?>">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="<?php echo base_url(); ?>about">About Us</a>
+      </li>
+
+    </ul>
+    <ul class="navbar-nav navbar-right">
+            <li class="nav-item">
+        <a class="nav-link" href="<?php echo base_url(); ?>posts/create">Create Post</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="text" placeholder="Search">
+      <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+<div class="container">
+
+
+<style>
+  .post-date {
+    background: #f4f4f4;
+    padding: 4px;
+    margin: 3px 0;
+    display: block;
+  }
+
+  .post-thumb {
+    width: 100%;
+  }
+</style>
