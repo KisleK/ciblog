bc6a8bc68fe2918993c081ea22d289e7fe88ffcc
KisleK <kisle.kuhn1@gmail.com>
2019-09-02 12:07:35 -0500

    fixed blog show issue

--- application/views/posts/view.php
+++ application/views/posts/view.php
@@ -7,7 +7,7 @@
 
 <?php if($this->session->userdata('user_id') == $post['user_id']): ?>
 	<hr>
-	<a class="btn btn-primary pull-left" href="<?php echo base_url(); ?>posts/edit/<?php echo $post['slug']; ?>">Edit</a>
+	<a class="btn btn-primary float-left" href="<?php echo base_url(); ?>posts/edit/<?php echo $post['slug']; ?>">Edit</a>
 	<?php echo form_open('/posts/delete/'.$post['id']); ?>
 		<input type="submit" value="Delete" class="btn btn-danger">
 	</form>
