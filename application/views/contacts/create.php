ad7a9b5bab95f9bcd9e0f9bac3a455bcde606799
KisleK <kisle.kuhn1@gmail.com>
2019-09-06 15:20:23 -0500

    mailgun update

--- application/views/contacts/create.php
+++ application/views/contacts/create.php
@@ -1,5 +1,5 @@
 <?php
-//composer require mailgun/mailgun-php:~1.7.2
+composer require mailgun/mailgun-php:~1.7.2
 # Include the Autoloader (see "Libraries" for install instructions)
 require 'vendor/autoload.php';
 use MailgunMailgun;
