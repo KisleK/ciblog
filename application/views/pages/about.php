9d6d6e8e19997eaffe3e4af582febe0428564caf
Kyle Kuhn <kkuhn@dealereprocess.local>
2019-11-11 17:13:17 -0600

    updated about us and home with general text

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -1,1 +1,24 @@
  <h2><?= $title; ?></h2> 
+
+ <br>
+ <br>
+
+ <h1>
+     This is version 1 of my MVP blog site hosted on heroku
+ </h1>
+
+ <p> There are still a few bugs to work out as images are not loading correctly for me as well as a couple bugs to fix for createing a user/ auto logging in. </p>
+
+<ul> MVP v.1
+    <li>
+        This blog was created with PHP, CodeIgniter, and initially PHP MyAdmin
+    </li>
+    <li>
+        Once the environment was set up and hosted on heroku the DB was switched to ClearDB MySQL
+    </li>
+</ul>
+
+ <ul> Feature Updates
+     <li>Version 2 will be able to successfully send an email. Currently researching Mailgun as my tool for emails</li>
+     <li>Bug fixes for uploading images</li>
+ </ul>
--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,1 +1,8 @@
  <h2><?= $title; ?></h2> 
+
+ <br>
+ <br>
+
+ <h1>
+     This is the MVP of my Blog site. Please create a log in and log in after. Submit a post if you feel like it.
+ </h1>
